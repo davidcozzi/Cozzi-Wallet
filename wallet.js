2859353987d8d50488308fcfea96a2c3ffbfca07
davidcozzi <146295863+davidcozzi@users.noreply.github.com>
2024-02-07 20:17:19 -0700

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -15,9 +15,9 @@
 function isPWA() { return Boolean((window.matchMedia && window.matchMedia('(display-mode: standalone)').matches)|| window.navigator.standalone); }
 
 import { ethers } from 'https://cdn.jsdelivr.net/npm/ethers@6.10.0/lib.commonjs/index.min.js';
-import { ImmortalStorage, LocalStorageStore, IndexedDbStore } from 'https://cdn.jsdelivr.net/gh/gruns/ImmortalDB@master/dist/immortal-db.min.js';
+import { ImmortalStorage, IndexedDbStore } from 'https://cdn.jsdelivr.net/gh/gruns/ImmortalDB@master/dist/immortal-db.min.js';
  
-const stores = [LocalStorageStore, IndexedDbStore];
+const stores = [IndexedDbStore];
 const db = new ImmortalStorage(stores);
 
 if(isPWA()){
